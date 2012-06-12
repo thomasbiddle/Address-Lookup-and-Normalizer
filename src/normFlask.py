887c91e5e1907808ef740ba788901b7c310e9414
Thomas Biddle <biddle.thomas@gmail.com>
2012-06-12 16:01:37 -0400

    Adding Address Separation

--- src/normFlask.py
+++ src/normFlask.py
@@ -7,9 +7,11 @@ app = Flask(__name__)
 @app.route("/", methods=['POST', 'GET'])
 def newTest():
 	if request.method == 'POST':
-		return normCLI.checkAdd(request.form['input'])
+		newAddress = normCLI.checkAdd(request.form['input'])
+		return newAddress.jsonAdd()
 	else:
-		return normCLI.checkAdd(request.args.get('input'))
+		newAddress = normCLI.checkAdd(request.args.get('input'))
+		return newAddress.jsonAdd()
 if __name__ == "__main__":
 	app.debug = True
 	app.run(host='0.0.0.0')
